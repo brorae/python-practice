50be7bb62ebbbd74b7d1e78e035b0ac45f0f8034
d <johrae_@naver.com>
2021-02-17 19:31:29 +0900

    210217

--- 1차원 배열/8958.py
+++ 1차원 배열/8958.py
@@ -4,9 +4,12 @@ n = int(input())
 
 for _ in range(n):
     arr = list(sys.stdin.readline().strip())
-    a = 0
-    while(arr[a]=='O'):
-        
-
-        if(arr[a]=='X'):
-            break
+    sum = 0
+    c = 1
+    for i in arr:
+        if (i=='O'):
+            sum += c
+            c += 1
+        if (i=='X'):
+            c = 1
+    print(sum)
